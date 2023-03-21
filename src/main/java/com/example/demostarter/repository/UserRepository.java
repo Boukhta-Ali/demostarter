b31f1bc89a6224b07a742a955078aa55abb77ba5
Ali Boukhta <ali.boukhta.partner@decathlon.com>
2023-03-21 17:07:24 +0100

    init

--- src/main/java/com/example/demostarter/repository/UserRepository.java
+++ src/main/java/com/example/demostarter/repository/UserRepository.java
@@ -3,8 +3,8 @@ package com.example.demostarter.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demostarter.domain.User;
+import com.example.demostarter.domain.Users;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
